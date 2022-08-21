45dd620e70c8a4adcec6736351524d7ceb1f549c
Daemontrit <slavikk1999@yandex.ru>
2022-08-21 13:43:10 +0300

    refactor code

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,41 +1,45 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class BonusServiceTest {
     @Test
-    void shouldCalculateRegisteredAndUnderLimit(){
-        BonusService service=new BonusService();
-        long amount=1000;
+    void shouldCalculateRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000;
         boolean registered = true;
-        long expected=30;
-        long actual=service.calculate(amount,registered);
-        assertEquals(expected,actual);
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
     }
+
     @Test
-    void shouldCalculateForRegisteredAndOverLimit(){
-        BonusService service=new BonusService();
-        long amount=1_000_000;
-        boolean registered=true;
-        long expected=500;
-        long actual=service.calculate(amount,registered);
-        assertEquals(expected,actual);
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
     }
+
     @Test
-    void shouldCalculateForNotRegisteredAndOverLimit(){
-        BonusService service=new BonusService();
-        long amount=1_000_000;
-        boolean registered =false;
-        long expected=500;
-        long actual=service.calculate(amount,registered);
-        assertEquals(expected,actual);
+    void shouldCalculateForNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
     }
+
     @Test
-    void shouldCalculateForNotRegisteredAndUnderLimit(){
-        BonusService service =new BonusService();
-        long amount=1000;
-        boolean registered=false;
-        long expected=10;
-        long actual=service.calculate(amount,registered);
-        assertEquals(expected,actual);
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
     }
 }
